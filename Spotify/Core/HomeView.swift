c60b71040b63ff366013c6faa76895539e923d74
Mahesh Behere <maheshbehere14@gmail.com>
2025-09-12 15:35:19 +0530

    MSA-2 initial HomeViewCommit

---
+++ Spotify/Core/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Spotify
+//
+//  Created by Mahesh Behere on 12/09/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
