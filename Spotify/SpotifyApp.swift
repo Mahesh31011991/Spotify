7d95355e761706a239aaa089602a718234bf3d4c
Mahesh Behere <maheshbehere14@gmail.com>
2025-09-12 11:32:18 +0530

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Mahesh Behere on 12/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
